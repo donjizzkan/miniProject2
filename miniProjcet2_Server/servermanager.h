1b68d2417036c1d8fdee8401368b27bf1c24a164
devwooms <devwooms@gmail.com>
2025-07-03 11:33:20 +0900

    server<->client working test finish

---
+++ miniProjcet2_Server/servermanager.h
@@ -0,0 +1,29 @@
+#ifndef SERVERMANAGER_H
+#define SERVERMANAGER_H
+
+#include <QObject>
+#include <QMessageBox>
+
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QNetworkInterface>
+#include <QHostAddress>
+
+#define PORT_NUM  51234
+
+class ServerManager : public QObject
+{
+    Q_OBJECT
+public:
+    ServerManager();
+private:
+    void run();
+    QTcpServer *tcpServer;
+    QList<QTcpSocket*> *clientSocketList;
+    QString getMyIP();
+
+private slots:
+    void clientConnect();
+};
+
+#endif // SERVERMANAGER_H

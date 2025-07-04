f1bd8a63582e9d3f51018fed672799321a59f2dd
donjizzkan <donjizzkan@gmail.com>
2025-07-04 16:55:08 +0900

    Server MultiThread

--- miniProject2/loginview.cpp
+++ miniProject2/loginview.cpp
@@ -17,8 +17,8 @@ LoginView::LoginView(QWidget *parent)
 
     // 소켓 연결
     // 192.168.2.235 => devwooms
-    // 192.168.137.1 => donjizzkan
-    socket->connectToHost("192.168.137.1",51234);
+    // 192.168.56.1 => donjizzkan
+    socket->connectToHost("192.168.56.1",51234);
     connect(socket, &QTcpSocket::connected, this, [](){
         qDebug("서버 연결 완료");
     });

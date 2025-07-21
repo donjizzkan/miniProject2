703b30b1c109e1c5730db9ff1d77f9c370b10574
devwooms <devwooms@gmail.com>
2025-07-21 15:38:13 +0900

    Search setting

--- miniProject2/views/homeview.h
+++ miniProject2/views/homeview.h
@@ -2,12 +2,29 @@
 #define HOMEVIEW_H
 
 #include <QWidget>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QGridLayout>
+#include <QSplitter>
+#include <QTabWidget>
+#include <QComboBox>
+#include <QTextBrowser>
+#include <QToolBox>
+#include <QListWidget>
+#include <QLabel>
+#include <QLineEdit>
+#include <QSpinBox>
+#include <QRadioButton>
+#include <QPushButton>
+#include <QSpacerItem>
+#include <QFrame>
+#include <QTimer>
+#include <QEvent>
+
+#include "coinsearchwidget.h"
+#include "coinsearchlineedit.h"
 
 class ChartsToolBox; // forward declaration
-
-namespace Ui {
-class HomeView;
-}
 
 class HomeView : public QWidget
 {
@@ -17,9 +34,44 @@ public:
     explicit HomeView(QWidget *parent = nullptr);
     ~HomeView();
 
+protected:
+    bool eventFilter(QObject *obj, QEvent *event) override;
+
 private:
-    Ui::HomeView *ui;
     ChartsToolBox *chartBox; // ChartsToolBox 인스턴스 - devwooms
+    
+    // UI 요소들
+    QSplitter *splitter;
+    QTabWidget *tapwidget;
+    QComboBox *comboBox;
+    QVBoxLayout *chartTab;
+    QTextBrowser *textBrowser;
+    QTextBrowser *purchasePrice;
+    QTextBrowser *gain;
+    QTextBrowser *gainPercent;
+    QTextBrowser *recentPrice;
+    QLineEdit *lineEdit;
+    QSpinBox *spinBox;
+    QSpinBox *spinBox_2;
+    QRadioButton *radioButton;
+    QRadioButton *radioButton_2;
+    QPushButton *pushButton;
+    QTextBrowser *orderType;
+    QTextBrowser *orderDate;
+    QTextBrowser *orderAmount;
+    QTextBrowser *orderPrice;
+    QTextBrowser *orderTotal;
+    QToolBox *chatting_ToolBox;
+    QListWidget *connect_listWidget;
+    QListWidget *oneByone_listWidget;
+    QListWidget *oneByMore_listWidget;
+
+    QLineEdit* searchLineEdit;
+    // CoinSearchLineEdit* searchLineEdit;
+    CoinSearchWidget *coinSearchWidget;
+    
+    void setupUI();
+    void connectSignal();
 };
 
 #endif // HOMEVIEW_H
